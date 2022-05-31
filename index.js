1731a7e7956fb2d806fcd7ead353da112869435b
traceymmbonelungahi <t.lungahi@gmail.com>
2022-05-31 20:41:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let messages = [];
+function writeCards(names, eventName) {
+    for (let n = 0; n < names.length; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages; 
+}
+function countDown() {
+    let count = 10;
+    while(count >-1) {
+        console.log(count--);
+    }
+}
